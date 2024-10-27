cb27dfd90a2bab5e2bdc5f85f2959cee9d04a5a2
Cansin Acarer <cansinacarer@users.noreply.github.com>
2024-10-27 01:19:36 -0400

    standardize naming to validate in tests

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,9 +3,9 @@ from app.validator import Email
 
 
 class TestEmail(unittest.TestCase):
-    def test_verify_valid_email(self):
+    def test_validate_valid_email(self):
         email = Email("cansinacarer@gmail.com")
-        result = email.verify()
+        result = email.validate()
         self.assertIsInstance(result, dict)
         self.assertEqual(result.get("email"), "cansinacarer@gmail.com")
         self.assertTrue(result.get("is_free_provider"))
@@ -16,34 +16,34 @@ class TestEmail(unittest.TestCase):
             "email provider confirmed that the email address is deliverable",
         )
 
-    def test_verify_valid_alias_email(self):
+    def test_validate_valid_alias_email(self):
         email = Email("cansinacarer+test@gmail.com")
-        result = email.verify()
+        result = email.validate()
         self.assertIsInstance(result, dict)
         self.assertTrue(result.get("is_alias"))
 
-    def test_verify_role_email(self):
+    def test_validate_role_email(self):
         email = Email("info@cansin.net")
-        result = email.verify()
+        result = email.validate()
         self.assertIsInstance(result, dict)
         self.assertTrue(result.get("is_role"))
 
-    def test_verify_catchall(self):
+    def test_validate_catchall(self):
         email = Email("info@cansin.net")
-        result = email.verify()
+        result = email.validate()
         self.assertIsInstance(result, dict)
         self.assertTrue(result.get("is_catch_all"))
         self.assertTrue(result.get("is_role"))
 
-    def test_verify_disposable(self):
+    def test_validate_disposable(self):
         email = Email("xcgn@gimpmail.com")
-        result = email.verify()
+        result = email.validate()
         self.assertIsInstance(result, dict)
         self.assertTrue(result.get("is_disposable"))
 
-    def test_verify_invalid_address(self):
+    def test_validate_invalid_address(self):
         email = Email("test23gf43w5f@cacarer.com")
-        result = email.verify()
+        result = email.validate()
         self.assertIsInstance(result, dict)
         self.assertEqual(result.get("status"), "invalid")
         self.assertEqual(
@@ -51,9 +51,9 @@ class TestEmail(unittest.TestCase):
             "email provider confirmed that email address does not exist",
         )
 
-    def test_verify_invalid_domain(self):
+    def test_validate_invalid_domain(self):
         email = Email("test@cansin.net.com")
-        result = email.verify()
+        result = email.validate()
         self.assertIsInstance(result, dict)
         self.assertEqual(result.get("status"), "invalid")
         self.assertEqual(
